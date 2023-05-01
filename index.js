050e0ffae89aaef6ad4d0b795c134bfdbeb359d4
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2023-05-01 19:37:52 +0530

    hiii

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app  = require('./app');
 const {connectDb} = require('./config/dbConnect')
 connectDb();
 
-const PORT= process.env.PORT|| 4001
+const PORT= process.env.PORT|| 8000
 
 app.listen(PORT , ()=>{
     console.log(`Server is high and runnig at ${PORT} `)
